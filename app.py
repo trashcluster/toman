cd4933036732d531f988261227a90d74b3918d7c
Debian <debian@toman.trashcluster.com>
2020-10-30 07:35:29 +0000

    update app.py

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ from flask_restful import Resource, Api
 import valve.rcon
 
 app = Flask(__name__)
+api=Api(app)
 
 valve.rcon.execute(("85.10.205.252",27015),"password","help")
 
-@app.route('/')
-def hello():
-    return 
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
+api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
     app.run()
