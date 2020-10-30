822f27706b1f3e2f103cfe681c4652b66446a897
Debian <debian@toman.trashcluster.com>
2020-10-30 23:50:16 +0000

    update app.py

--- app.py
+++ app.py
@@ -1,22 +1,66 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-import valve.rcon
+#from valve.rcon import *
+import pymongo
+import dns
+import os
+import sys
 
+#Init Flask App
 app = Flask(__name__)
+#Init Flask-restful api
 api=Api(app)
 
-valve.rcon.execute(("85.10.205.252",27015),"password","help")
+try:  
+   os.environ['MONGODBURL']
+   MongoDbAccessLink = os.getenv('MONGODBURL')
+except KeyError: 
+   print ("ERROR, Please pass MongoDB full url in the system environment variables")
+   sys.exit(1)
 
-class HelloWorld(Resource):
+
+#Init mongodb connection to database
+mogodbclient = pymongo.MongoClient(MongoDbAccessLink)
+#Init database "tomandb" on "atlasclient"
+db = mogodbclient.tomandb
+
+#valve.rcon.execute(("85.10.205.252",27015),"password","help")
+
+################################## USER MANAGMENT ####################################
+
+# Init User collection/table
+# print("init UserCol")
+# UserCol = db.UserCol
+# print("insert data in userCol")
+# dict = { "USER": "TESTUSER" , "HAHA": "PROMP" }
+# print(dict)
+# x = UserCol.insert_one(dict).inserted_id
+# print( "id", x )
+# for x in UserCol.find():
+#     print(x)
+
+#Create a user, no right managment implemented for now.
+# UserCreate endpoint
+class UserCreate(Resource):
+    # GET request, doe snot perform any actions
     def get(self):
-        return {'hello': 'world'}
+        return {'403 error': 'use this endpoint to create a user, send a put request'}, 403
+
+    # PUT request, create a user entry in the database
     def put(self):
-        putdata = request.form['ayy']
-        return {'put' : putdata}
+        putdata = request.form['username']
+        #x = UserCol.insert_one(putdata).inserted_id
+        print(x)
+        #return {'userid' : x, 'username' : putdata}, 200
+        return {'userid' : 'YEET', 'username' : putdata}, 200
+
+class UserList(Resource):
+    def get(self):
+        return {UserCol.find()}, 200
 
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(UserCreate, '/api/v1/user/create')
+api.add_resource(UserList, '/api/v1/user/list')
 
 if __name__ == '__main__':
     app.run()
-
