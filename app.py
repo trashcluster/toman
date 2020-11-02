ae660b2a3eca5c51b7f547ba412069aba0820f44
Debian <debian@toman.trashcluster.com>
2020-11-02 08:41:25 +0000

    update app, add database class

--- app.py
+++ app.py
@@ -1,48 +1,19 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, fields, marshal_with
 #from valve.rcon import *
-import pymongo
-import dns
-import os
-import sys
 from email.utils import formatdate
 import time
+# Database class from the database.py file
+from database import Database
 
 #Init Flask App
 app = Flask(__name__)
 #Init Flask-restful api
 api=Api(app)
 
-try:  
-   os.environ['MONGODBURL']
-   MongoDbAccessLink = os.getenv('MONGODBURL')
-except KeyError: 
-   print ("ERROR, Please pass MongoDB full url in the system environment variables")
-   sys.exit(1)
-
-
-#Init mongodb connection to database
-mogodbclient = pymongo.MongoClient(MongoDbAccessLink)
-#Init database "tomandb" on "atlasclient"
-#db = mogodbclient.tomandb
-
 #valve.rcon.execute(("85.10.205.252",27015),"password","help")
 
 ################################## USER MANAGMENT ####################################
-
-# Init User collection/table
-# print("init UserCol")
-# UserCol = db.UserCol
-# print("insert data in userCol")
-# dict = { "USER": "TESTUSER" , "HAHA": "PROMP" }
-# print(dict)
-# 
-# print( "id", x )
-# for x in UserCol.find():
-#     print(x)
-
-### Def Database column
-#UserCol = db.UserCol
 
 ### Def user fields
 user_fields = {
@@ -84,26 +55,10 @@ role_fields = {
     'role_perm_id': fields.Integer,
 }
 
-### _id : mongodb id
-def db_get_data_id(database, column, searchid):
-    db = mogodbclient[database]
-    col = db[column]
-    for x in col.find({},{ "_id": searchid}):
-        return x
-
-### database : database to use, column : column/table to use, dict : dictionary to insert into the database
-### mongodbclient is defined at the top of the app
-def db_put_data(database, column, dict):
-    db = mogodbclient[database]
-    col = db[column]
-    x = col.insert_one(dict).inserted_id
-    return {'id': x, 'inserted data': db_get_data_id(database, column, x)}
-
-
-
 #Create a user, no right managment implemented for now.
 # UserCreate endpoint
 class UserCreate(Resource):
+    Database.initialize("tomandb")
     # GET request, does not perform any actions
     def get(self):
         return {'405 error': 'use this endpoint to create a user, send a put request'}, 405
@@ -111,27 +66,19 @@ class UserCreate(Resource):
     # PUT request, create a user entry in the database
     def put(self):
         putdata = request.form['username']
-        userid = UserCol.insert_one(putdata).inserted_id
-        print(userid)
-        #return {'userid' : x, 'username' : putdata}, 200
-        return {'userid' : 'YEET', 'username' : putdata}, 200
-    
-    @marshal_with(user_fields, envelope='resource')
-    def put(self, **kwargs):
-        db_put_data(tomandb, user, )
-        return
-    
-class UserEdit(Resource):
-    def get(self):
-        userinfo
+        putdata += request.form['name']
+        putdata += request.form['firstname']
+        putdata += request.form['pseudonym']
 
-class UserList(Resource):
-    def get(self):
-        return {UserCol.find()}, 200
+        ### Database put data
+        #return {'userid' : 'YEET', 'username' : putdata}, 200
+        Database.insert("UserCol", {'userid' : 'YEET', 'username' : putdata})
+        return putdata, 200
+
 
 
 api.add_resource(UserCreate, '/api/v1/user/create')
-api.add_resource(UserList, '/api/v1/user/list')
+#api.add_resource(UserList, '/api/v1/user/list')
 
 if __name__ == '__main__':
     app.run()
---
+++ database.py
@@ -0,0 +1,43 @@
+import pymongo
+import os
+import sys
+import dns
+
+# stop the program if no mongodb url is detected
+try:  
+    os.environ['MONGODBURL']
+    MongoDbAccessLink = os.getenv('MONGODBURL')
+except KeyError: 
+    print ("ERROR, Please pass MongoDB full url in the system environment variables")
+    sys.exit(1)
+
+# Class used to interact with the database without having to initialize it in the main app
+class Database:
+    database = None
+
+    # Initialize database, pass a database name
+    @classmethod
+    def initialize(cls, database):
+        client = pymongo.MongoClient(MongoDbAccessLink)
+        cls.database = client[database]
+
+    # Input a dictionary "data" into the specified "collection"
+    @classmethod
+    def insert(cls, collection, data):
+        cls.collection = cls.database[collection]
+        cls.collection.insert_one(data)
+
+    @classmethod
+    def read(cls, collection, query):
+        return_dict=[]
+        cls.collection = cls.database[collection]
+        x = cls.collection.find(query)
+        for i in x:
+            return_dict.append(i)
+        return return_dict
+
+        
+    #@classmethod
+    #def update(cls, objid, key, param, data):
+    #    cls.database.collection.update(  { _id:objid} , { $set: { key.param : data  } }
+
